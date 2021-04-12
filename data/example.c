983b0c7451d5a394d9a039a7490677274245eb04
CharlesHan24 <hanwenchen24@gmail.com>
2021-04-12 14:50:33 +0800

    fix

--- data/example.c
+++ data/example.c
@@ -1,14 +1,13 @@
-int a[5][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
 
-void add(int a[]){
-    a[0] = a[0] + 1;
-    a[1] = a[1] + 1;
-    a[2] = a[2] + 1;
-}
+int main()
+{
 
-int main(){
-    add(a[1]);
-    add(a[4]);
-    putarray(25, a);
+     if(1 == 1)
+        putint(1);
+        else
+        {
+            putint(1);
+        }
+
     return 0;
 }
